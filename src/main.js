c5a7f462e5c88b26019b5109cafc1e17cd99ac7b
AngryBirdsFighting <15903557434@sina.cn>
2018-08-07 17:40:32 +0800

    商品列表完成

--- src/main.js
+++ src/main.js
@@ -8,13 +8,13 @@ import router from './router'
 // Vue.use(Vant) // 全局引入  不提倡
 // import vueAwesomeSwiper from 'vue-awesome-swiper'
 // import 'swiper/dist/css/swiper.css'
-import {Button, Row, Col, Swipe, SwipeItem, Lazyload, List, Field , Tab,Tabs, Toast, NavBar} from 'vant' // 按需引入
+import {Button, Row, Col, Swipe, SwipeItem,PullRefresh, Lazyload, List, Field , Tab,Tabs, Toast, NavBar} from 'vant' // 按需引入
 import '@/assets/icon/iconfont.css'
 import instance from './util/http.js'
 
 
 Vue.prototype.$axios = instance;
-Vue.use(Button).use(Row).use(Col).use(Swipe)
+Vue.use(Button).use(Row).use(Col).use(Swipe).use(PullRefresh)
 .use(Tab).use(Tabs).use(SwipeItem).use(Lazyload)
 .use(List).use(NavBar).use(Field).use(Toast) //.use(vueAwesomeSwiper)
 Vue.config.productionTip = false
